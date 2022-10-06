aeb799a492ff5627dc66c0d6ee40ce8e97bd11a6
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-10-05 20:22:58 -0700

    adding in jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,76 @@
-// // ## Acceptance Criteria
+// // // ## Acceptance Criteria
 
-// ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar DONE
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours DONE
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock Done
-// THEN I can enter an event Done
-// WHEN I click the save button for that timeblock Done
-// THEN the text for that event is saved in local storage Done
-// WHEN I refresh the page
-// THEN the saved events persist
-// $(document).ready(function () { 
+// // ```md
+// // GIVEN I am using a daily planner to create a schedule
+// // WHEN I open the planner
+// // THEN the current day is displayed at the top of the calendar DONE
+// // WHEN I scroll down
+// // THEN I am presented with timeblocks for standard business hours DONE
+// // WHEN I view the timeblocks for that day
+// // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// // WHEN I click into a timeblock Done
+// // THEN I can enter an event Done
+// // WHEN I click the save button for that timeblock Done
+// // THEN the text for that event is saved in local storage Done
+// // WHEN I refresh the page
+// // THEN the saved events persist
+// // $(document).ready(function () { 
 
-// });
+// // });
+var currentDate = new Date() //date is a built in JavaScript Object (some objects need "new" to access them)
+var currentHour = currentDate.getHours()
+
+
+
+// var timeBlocksArray = document.querySelectorAll(".time-block") //array to get all elements of this else
+// console.log(timeBlocksArray) //each timeblock is its own object (represents HTML on the page)
+
+// timeBlocksArray = Array.from(timeBlocksArray) // 
+// //only needed to fix a bug
+// for (let i = 0; i < timeBlocksArray.length; i++) { //i defines a temporary var; it initializes it to 0. i< defines the end condition.
+//     //i++ tells me how i changes on every instance of this loop. ++ increases the value of i by 1. 
+//     //text += cars[i] + "<br>"; 
+
+//     // var sampleArray = ["a", "b", "c", 1, 2, 3]
+//     // sampleAarray[2] === "c"
+//     // sampleArray[4]
+//     var timeBlock = timeBlocksArray[i];
+
+//     if(currentHour > parseInt(timeBlock.id)) {
+//         timeBlock.classList.add("past") //adds class to timeblock in the past
+    
+//     }
+//     else if(currentHour === parseInt(timeBlock.id)){
+//         timeBlock.classList.add("present")
+
+//     }
+//     else {
+//         timeBlock.classList.add("future")
+
+//     }
+//     }
+
+
+    $(".time-block").each(function(){ //$ is the equivalent of document.querySelectorAll in Vanilla JS
+        //.each is the equivalent of a for loop
+         //this refers to each element in the array
+         if(currentHour > parseInt($(this).attr("id"))) {
+            $(this).addClass("past") //adds class to timeblock in the past
+            //$(this) is each element in the array
+        
+        }
+        else if(currentHour === parseInt($(this).attr("id"))){
+            $(this).addClass("present")
+    
+        }
+        else {
+            $(this).addClass("future")
+    
+        }
+        
+        
+    })
+
 $("#currentDay").text(moment().format("dddd, MMMM Do")); //adds the current date
 
 function saveBtnHandler() {
